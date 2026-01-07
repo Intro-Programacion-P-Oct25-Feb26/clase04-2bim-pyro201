f1f1c5e5a95364ad1eead61d677651e5217d50fc
jonathan <jonathanjavier2004@hotmail.com>
2026-01-07 12:42:10 -0500

    funcion

---
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo044.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos02;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo044 {
+
+    public static void main(String[] args) {
+        // 
+
+        int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        int[][] arreglo3;
+
+        arreglo3 = matriz3(arreglo1, arreglo2);
+
+        obtenerReporte(arreglo1);
+        obtenerReporte(arreglo2);
+        obtenerReporte(arreglo3);
+    }
+
+    public static int[][] matriz3(int[][] a, int[][] b) {
+        int[][] matriz = new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+
+                matriz[i][j] = obtenerMultiplicacion(a[i][j],
+                        b[i][j]);
+            }
+        }
+
+        return matriz;
+    }
+
+    public static void obtenerReporte(int[][] arreglo1) {
+        String cadena = "";
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, arreglo1[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    public static int obtenerMultiplicacion(int a, int b) {
+        int operacion;
+        operacion = a * b;
+        return operacion;
+
+    }
+
+}
