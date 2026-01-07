31f31343bf9b460488d5a5597e87f3fcf7798606
jonathan <jonathanjavier2004@hotmail.com>
2026-01-07 12:20:27 -0500

    arreglos

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo043.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo043.java
@@ -12,15 +12,25 @@ package ejemplos02;
 public class Ejemplo043 {
 
     public static void main(String[] args) {
-        // 
-        obtenerSuma(10, 30); // se invoca al método (procedimiento) obtenerSuma
-        
+        //
+        int[] valores1={10,100,1000,20,200,2000};
+        int[] valores2={1000,1001,1002,1003,1004,1005};
+        for (int f =0; f < valores1.length;f++){
+            int v1= valores1[f];
+            int v2= valores2[f];
+             obtenerSuma(v1,v2);
+        }
+        /* este codigo toma los valores correspondientes en cada arreglo y
+        se los envia a el procedimiento obtenerSuma el cual los suma y presenta
+        el resultado en la pantalla
+        */
     }
         
     public static void obtenerSuma(int a, int b){
         int suma;
         suma = a + b;
         System.out.printf("El valor de la suma es: %d\n", suma);
+        
         
     }
     
